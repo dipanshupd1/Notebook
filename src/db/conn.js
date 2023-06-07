63f7399b2e9813514cac1ed3b9845afe129f6be3
dipanshu prasad <dipanshupd123@gmail.com>
2023-06-07 23:39:13 +0530

    port added to ,env file-3

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const dotenv=require('dotenv')
 dotenv.config()
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', true)
 mongoose.connect(process.env.DB_URL).then(()=>{
     console.log("connected to database")
 }).catch((err)=>{
-    console.log("connection unsuccessful")
+    console.log("connection unsuccessful",err)
 })
